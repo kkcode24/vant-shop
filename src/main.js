138cf437ce9f7f5a9b59be5b6e8d6d039896b527
yk1314 <1057192097@qq.com>
2019-09-17 11:48:12 +0800

    版本回退

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+import App from './App'
+import router from './router'
+
+Vue.use(Vant);
+
+
+Vue.config.productionTip = false
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
