14b8f1a31ddc3012e43b912bdaa829ad69144acd
kkcode24 <kkcode@126.com>
2019-10-02 22:47:44 +0800

    微信支付

--- src/views/order/submit.vue
+++ src/views/order/submit.vue
@@ -29,24 +29,33 @@
         </p>
         <div class="price-panel__total van-hairline--top">
     合计：<span class="price-panel__amount theme-color">
-      ¥255.00</span></div>
+      ¥{{order.discountTotalPrice}}</span></div>
       </div>
     </div>
 
     <van-submit-bar :loading="loading" :price="order.totalPrice*100" button-text="提交订单" @submit="onSubmit" />
+    <van-action-sheet
+      v-model="show"
+      :actions="actions"
+      cancel-text="取消"
+      @select="onSelect"
+      @cancel="onCancel"
+    />
   </div>
 </template>
 
 <script>
-import { saveOrder } from "@/api/order";
+import { saveOrder,billWXPay } from "@/api/order";
 export default {
   name: "submitOrder",
   data() {
     return {
+      show: false,
       loading: false,
       currentContact: {},
       addresslist: [],
       orderGoodList: [],
+      orderId: 21,
       order: {
         addressId: null,
         // 给卖家留言
@@ -57,7 +66,11 @@ export default {
         totalPrice: 0,
         // 收费金额（取折后总金额）
         discountTotalPrice: 0
-      }
+      },
+      actions: [
+        { name: '微信支付',payType: 'wx' },
+        { name: '支付宝支付',payType:'ali' }
+      ]
     };
   },
   beforeRouteLeave(to, from, next) {
@@ -100,11 +113,66 @@ export default {
       });
     },
     onSubmit() {
+      this.show = true;
+      return false;
       this.loading = true;
       saveOrder({ ...this.order, orderFruit: this.orderGoodList }).then(res => {
         this.loading = false;
-        console.log(res);
+        if(res.code === 0){
+          this.orderId = res.data;
+        }
       });
+    },
+    onSelect(item) {
+      this.show = false;
+      if(item.payType==='wx'){ // 微信支付
+          billWXPay(this.orderId)
+            .then((res) => {
+              function onBridgeReady(e, d) {
+                WeixinJSBridge.invoke(
+                  'getBrandWCPayRequest', {
+                    "appId": d.appid, //公众号名称，由商户传入     
+                    "timeStamp": d.timestamp + '', //时间戳，自1970年以来的秒数     
+                    "nonceStr": d.noncestr, //随机串     
+                    "package": d.package,
+                    "signType": "MD5", //微信签名方式：     
+                    "paySign": d.sign //微信签名 
+                  },
+                  function(res) {
+                    if (res.err_msg == "get_brand_wcpay_request:ok") {
+                      setTimeout(function() {
+                        that.$toast('支付成功')
+                        //that.$router.push('/order/pay/success')
+                      }, 500);
+                    }
+                    if (res.err_msg == "get_brand_wcpay_request:cancel") {
+                      this.$toast('支付已取消')
+                    }
+                  });
+              }
+              if (res.Code === 0) {
+                var d = res.data
+                if (typeof WeixinJSBridge == "undefined") {
+                  if (document.addEventListener) {
+                    document.addEventListener('WeixinJSBridgeReady', function(e, d) { onBridgeReady(e, d) }, false);
+                  } else if (document.attachEvent) {
+                    document.attachEvent('WeixinJSBridgeReady', function(e, d) { onBridgeReady(e, d) });
+                    document.attachEvent('onWeixinJSBridgeReady', function(e, d) { onBridgeReady(e, d) });
+                  }
+                } else {
+                  onBridgeReady(undefined, d)
+                }
+              }
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+      }else if(item.payType==='ali'){ // 支付宝支付
+        this.$toast('开发中')
+      }
+    },
+    onCancel(){
+      this.$toast('已取消支付');
     }
   },
   computed: {
