0ead0720bf3921c2f7dbe6e387d9529e7eff604f
chao-code <m13271330767@163.com>
2019-10-02 21:45:16 +0800

    购物车

--- src/api/class.js
+++ src/api/class.js
@@ -7,3 +7,7 @@ export function getFriutList(data) {
 export function getFriutListById(data) {
   return request({ url: '/fruit/queryByFruitTypeId', method: 'get', params: data })
 }
+//根据id 获取分类以及水果详情
+export function getNromById(data) {
+  return request({ url: '/fruit/' + data.id + '/getForm', method: 'get'})
+}
